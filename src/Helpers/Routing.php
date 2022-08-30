b4d20d684f713d57f6c810834eac0207a336be75
Waad Mawlood <w12345678996392@gmail.com>
2022-08-30 15:10:17 +0300

    fix model bind in controller

--- src/Helpers/Routing.php
+++ src/Helpers/Routing.php
@@ -25,21 +25,21 @@ class Routing extends OriginalRegistrar
 
     protected function addResourceChange($name, $base, $controller, $options)
     {
-        $uri = $this->getResourceUri($name).'/change/{id}';
+        $uri = $this->getResourceUri($name).'/change/{'.$base.'}';
         $action = $this->getResourceAction($name, $controller, 'change', $options);
         return $this->router->post($uri, $action);
     }
 
     protected function addResourceDelete($name, $base, $controller, $options)
     {
-        $uri = $this->getResourceUri($name).'/delete/{id}';
+        $uri = $this->getResourceUri($name).'/delete/{'.$base.'}';
         $action = $this->getResourceAction($name, $controller, 'delete', $options);
         return $this->router->delete($uri, $action);
     }
 
     protected function addResourceRestore($name, $base, $controller, $options)
     {
-        $uri = $this->getResourceUri($name).'/restore/{id}';
+        $uri = $this->getResourceUri($name).'/restore/{'.$base.'}';
         $action = $this->getResourceAction($name, $controller, 'restore', $options);
         return $this->router->post($uri, $action);
     }
