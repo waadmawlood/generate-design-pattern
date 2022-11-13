9fff8c217a884a9bfb6b5529e2ca82aad671f61f
Waad Mawlood <44348636+waadmawlood@users.noreply.github.com>
2022-11-13 15:49:29 +0300

    Update PatternServiceProvider.php

--- src/PatternServiceProvider.php
+++ src/PatternServiceProvider.php
@@ -1,11 +1,11 @@
 <?php
 
-namespace Waad\RepoMedia;
+namespace Waad\Repository;
 
 use Waad\RepoMedia\GenerateRepo;
 use Illuminate\Support\ServiceProvider;
 
-class PatternServiceProvider extends ServiceProvider
+class RepositoryServiceProvider extends ServiceProvider
 {
     /**
      * Register services.
@@ -14,18 +14,6 @@ class PatternServiceProvider extends ServiceProvider
      */
     public function register()
     {
-
-        // $this->publishes([
-        //     __DIR__ . '/Repositories/BaseRepository.php' => base_path('app/Http/Repositories/BaseRepository.php'),
-        // ]);
-
-        // $this->publishes([
-        //     __DIR__ . '/Models/Media.php' => base_path('app/Models/Media.php'),
-        // ]);
-
-        $this->publishes([
-            __DIR__.'/../database/migrations/create_media_table.php' => database_path('migrations/'.date('Y_m_d_His', time()).'_create_media_table.php'),
-        ], 'migrations');
 
         if ($this->app->runningInConsole()) {
             $this->commands([
